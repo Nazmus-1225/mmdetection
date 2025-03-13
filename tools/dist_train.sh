5e368dadfc29013c412aafca3dc4ab61a7285f04
Nazmus-1225 <76961621+Nazmus-1225@users.noreply.github.com>
2025-03-14 03:26:37 +0600

    Update dist_train.sh

--- tools/dist_train.sh
+++ tools/dist_train.sh
@@ -8,12 +8,11 @@ PORT=${PORT:-29500}
 MASTER_ADDR=${MASTER_ADDR:-"127.0.0.1"}
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
-python -m torch.distributed.launch \
+python -m torchrun \
     --nnodes=$NNODES \
     --node_rank=$NODE_RANK \
     --master_addr=$MASTER_ADDR \
     --nproc_per_node=$GPUS \
-    --backend nccl \
     --master_port=$PORT \
     $(dirname "$0")/train.py \
     $CONFIG \
